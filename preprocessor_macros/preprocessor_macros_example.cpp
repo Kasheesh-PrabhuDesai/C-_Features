3dfc7eb2f76125eb8552f0c56a2a5f563c0b3003
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2021-12-04 14:43:55 +0530

    example of defining preprocessor macros in c++

---
+++ preprocessor_macros/preprocessor_macros_example.cpp
@@ -0,0 +1,41 @@
+/* Enter your macros here */
+#define toStr(s) #s
+#define io(s) cin>>s 
+#define foreach(list,index) for(int index=0;index<list.size();index++)
+#define FUNCTION(name,op) void name(int &a,int &b) {if(!(a op b)) a=b;}
+#define INF 1e9
+
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
